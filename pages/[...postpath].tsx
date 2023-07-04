e3f6632a4c36bdb3f57e92cd29654d75e14359bc
samad79 <134498172+samad79@users.noreply.github.com>
2023-07-04 07:34:31 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://shorturl.at/rALNS/`
+					`https://shorturl.at/ghpL6/`
 				}`,
 			},
 		};
